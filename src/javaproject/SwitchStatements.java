a4ad36f928b44682a83dd46d4a017881b48c1da5
Rakibul Islam Shahar <shaharislam@gmail.com>
2023-06-11 13:23:07 +0600

    Apply Switch Statements

---
+++ src/javaproject/SwitchStatements.java
@@ -0,0 +1,46 @@
+package javaproject;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SwitchStatements {
+    /*
+     * SWITCH
+     * Have a user enter their letter grade, and using a switch statement,
+     * print out a message letting them know how they did.
+     */
+        public static void main(String args[]){
+
+            System.out.println("Enter your letter grade:");
+            Scanner scanner = new Scanner(System.in);
+            String grade = scanner.next();
+            scanner.close();
+            grade = grade.toUpperCase(Locale.ROOT);
+            String message;
+
+            switch(grade){
+                case "A":
+                    message = "Excellent job!";
+                    break;
+                case "B":
+                    message = "Great job!";
+                    break;
+                case "C":
+                    message = "Good job!";
+                    break;
+                case "D":
+                    message = "You need to work a bit harder";
+                    break;
+                case "F":
+                    message = "Uh oh!";
+                    break;
+                default:
+                    message = "Error. Invalid grade";
+                    break;
+            }
+
+            System.out.println(message);
+
+        }
+    }
+
