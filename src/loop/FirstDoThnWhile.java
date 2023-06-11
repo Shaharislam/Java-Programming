b0213a74936f868832c313c7d0ba870856559c3c
Rakibul Islam Shahar <shaharislam@gmail.com>
2023-06-11 19:34:24 +0600

    Do-While
    Do-While

---
+++ src/loop/FirstDoThnWhile.java
@@ -0,0 +1,40 @@
+package loop;
+import java.util.Scanner;
+
+public class FirstDoThnWhile {
+
+
+    /*
+     * DO WHILE LOOP:
+     * Write a program that allows a user to enter two numbers,
+     * and then sums up the two numbers. The user should be able to
+     * repeat this action until they indicate they are done.
+     */
+
+
+        public static void main(String args[]){
+
+            Scanner scanner = new Scanner(System.in);
+
+            boolean again;
+
+            do{
+                System.out.println("Enter the first number");
+                double num1 = scanner.nextDouble();
+
+                System.out.println("Enter the second number");
+                double num2 = scanner.nextDouble();
+
+                double sum = num1 + num2;
+
+                System.out.println("The sum is " + sum);
+
+                System.out.println("Would you like to start over?");
+                again = scanner.nextBoolean(); // input true / false
+
+            } while(again);
+
+            scanner.close();
+        }
+    }
+
