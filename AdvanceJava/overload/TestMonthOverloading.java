a4fbdd3aab422667092193e35896e3b6b7203294
Rakibul Shahar <78203754+Shaharislam@users.noreply.github.com>
2023-06-13 10:08:15 +0600

    Overloading Test
    Overloading Test

---
+++ AdvanceJava/overload/TestMonthOverloading.java
@@ -0,0 +1,8 @@
+package overload;
+
+public class TestMonthOverloading {
+    public static void main(String args[]){
+        System.out.println(MonthOverLoad.getMonth(2));
+        System.out.println(MonthOverLoad.getMonth("January"));
+    }
+}
