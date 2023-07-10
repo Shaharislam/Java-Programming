4b5da4926f5a3871c5c850ca6b78b6ed804f2440
Rakibul Shahar <shaharislam@gmail.com>
2023-07-10 19:25:57 +0600

    Super Constructor

--- AdvanceJava/methods/SuperClass.java
+++ AdvanceJava/methods/SuperClass.java
@@ -2,4 +2,19 @@ package methods;
 
 public class SuperClass {
     int x =10;
+
+    SuperClass(){
+        System.out.println(" I am Super Class Constructor without Parameter");
+    }
+
+    SuperClass(int age){
+        System.out.println(" I am Super Class Constructor with Age Parameter:= "+age);
+    }
+    void displayInformation(){
+        System.out.println("I am From Super Class.");
+    }
+
+    void message(){
+        System.out.println("I am From Super class message function");
+    }
 }
