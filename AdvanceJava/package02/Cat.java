d9cf65179fa83f59721e29eb7768404bf3f73c00
Rakibul Shahar <shaharislam@gmail.com>
2023-07-10 23:35:28 +0600

    Update Package to Package Same Class Access

--- AdvanceJava/package02/Cat.java
+++ AdvanceJava/package02/Cat.java
@@ -5,5 +5,8 @@ public class Cat {
     public static void main(String[] args) {
         Cow cw = new Cow(); // Class from Another Package
         Duck dk = new Duck();
+
+        Dog dog = new Dog(); // same package class 
+        package01.Dog dog1 = new package01.Dog(); // Same Class name from different package access system.
     }
 }
