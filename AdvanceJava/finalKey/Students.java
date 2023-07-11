7b3041cb7605ea24bc682a0d541f9acbb08cd702
Rakibul Shahar <shaharislam@gmail.com>
2023-07-11 08:18:34 +0600

    Use of Final Key Word

---
+++ AdvanceJava/finalKey/Students.java
@@ -0,0 +1,10 @@
+package finalKey;
+
+public class Students extends University{
+     // display() method inherit auto
+    // final method doesn't override but inherit
+    void display1() {
+        System.out.println();
+        System.out.println("I am from Students Class.");
+    }
+}
